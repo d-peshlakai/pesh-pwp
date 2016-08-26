b6a16089d865e4a0f6c0fe01eda480ef62437650
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-25 21:27:45 -0600

    setting up pwp website with boostrap and header

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -10,6 +10,59 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (required for Bootstap's JS plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		</head>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<!--Header and Navigation bar-->
+			<header>
+				<div class="container">
+					<!--Bootstrap Navigation Bar-->
+					<nav class="navbar navbar-inverse">
+						<!-- logo and mobile toggle button get grouped together for better mobile display -->
+						<div class="navbar-header">
+							<!-- this is the mobile menu button -->
+							<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+								<span class="sr-only">main menu</span>
+								<span class="glyphicon glyphicon-menu-hamburger"></span>
+							</button>
+							<a class="navbar-brand" href="#">Diane Peshlakai</a>
+						</div>
+						<!-- here are your main nav links, grouped for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<a class="navbar-icon">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#"></a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Contact</a></li>
+								</div>
+						</header>
+						<main>
+							<div class="container">
+										<div class="jumbotron">
+											<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+											<img src="images/computer-shot.jpg" class="img-fluid" alt="computer">
+
+								</div>
+								</div>
+
+
+
+</main>
 
 
 	</html>
