f9724a1773a14e214a05007ed2063a722149ae0d
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-05 13:09:41 -0600

    added footer info to footer.php file

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -117,35 +117,7 @@
 													</div>
 											</div>
 									</section>
-											<!-----Footer Content----------->
-			<footer>
-				<div class="social">
-					<div class="row">
-						<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-							<ul class="list-inline text-center">
-								<li>
-									<span>
-										<a href="#">
-											<i class="fa fa-facebook" aria-hidden="true"></i>
-											<i class="fa fa-twitter" aria-hidden="true"></i>
-											<i class="fa fa-lg fa-github" aria-hidden="true"></i></a>
-								</span>
-								</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				<div class="social">
-					<div class="row">
-						<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-							<ul class="list-inline text-center">
-								<li>
-								</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</footer>
+
 	</body>
 
 
--- public_html/documentation/partials/footer.php
+++ public_html/documentation/partials/footer.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: STEMulus
- * Date: 9/5/2016
- * Time: 1:06 PM
- */
+<!-----Footer Content----------->
+			<footer>
+				<div class="social">
+					<div class="row">
+						<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+							<ul class="list-inline text-center">
+								<li>
+									<span>
+										<a href="#">
+											<i class="fa fa-facebook" aria-hidden="true"></i>
+											<i class="fa fa-twitter" aria-hidden="true"></i>
+											<i class="fa fa-lg fa-github" aria-hidden="true"></i></a>
+								</span>
+								</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				<div class="social">
+					<div class="row">
+						<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+							<ul class="list-inline text-center">
+								<li>
+								</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</footer>
