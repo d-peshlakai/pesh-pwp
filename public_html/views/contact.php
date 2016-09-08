11f6e7d75daca5034c3d6d2ca1c42944753044d3
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-08 09:13:20 -0600

    inputed the recaptcha link

--- public_html/views/contact.php
+++ public_html/views/contact.php
@@ -24,7 +24,8 @@
 		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
+		<!--Google reCAPTCHA-->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -95,29 +96,32 @@
 
 								<!--Begin Contact Form-->
 			<main>
-				<div class="container">
-			<form id="contact-form" action="php/mailer.php" method="post">
-				<div class="form-group">
-					<label for="name">Name <span class="text-danger">*</span></label>
-						<div class="input-group">
+
+				<div class="container form-wrap">
+					<div class="row">
+						<div class="col-sm-6">
+							<form id="contact-form" action="php/mailer.php" method="post">
+								<div class="form-group">
+									<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
 							<div class="input-group-addon">
 								<i class="fa fa-user" aria-hidden="true"></i>
 							</div>
 							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 						</div>
 					</div>
-	<div class="form-group">
-		<label for="email">Email <span class="text-danger">*</span></label>
-		<div class="input-group">
-			<div class="input-group-addon">
+			<div class="form-group">
+				<label for="email">Email <span class="text-danger">*</span></label>
+				<div class="input-group">
+				<div class="input-group-addon">
 				<i class="fa fa-envelope" aria-hidden="true"></i>
 			</div>
 			<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 		</div>
 	</div>
-	<div class="form-group">
-		<label for="subject">Subject</label>
-		<div class="input-group">
+			<div class="form-group">
+			<label for="subject">Subject</label>
+			<div class="input-group">
 			<div class="input-group-addon">
 				<i class="fa fa-pencil" aria-hidden="true"></i>
 			</div>
@@ -134,8 +138,9 @@
 		</div>
 	</div>
 
-									<!-- reCAPTCHA -->
-	<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+									<!-- google recaptcha-->
+								<div class="g-recaptcha" data-sitekey="6LdQsykTAAAAANPVJzGdnDI1Izn0VJ438a7G0CIQ"></div>
+
 
 	<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 	<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
