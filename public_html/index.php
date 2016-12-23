1a1e49f7c91f44dc73451807d18ccfe882cb71d2
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-12-23 09:39:56 -0700

    under maintenance input new nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -58,33 +58,37 @@
 	<body class="sfooter">
 		<div class="sfooter-content">
 			<!--Header and Navigation bar-->
-			<header>
-				<div class="box-container">
-					<!--Bootstrap Navigation Bar-->
-					<nav class="navbar navbar-inverse">
-						<!-- logo and mobile toggle button get grouped together for better mobile display -->
-						<div class="navbar-header">
-							<!-- this is the mobile menu button -->
-							<button class="navbar-toggle collapsed" type="button" data-toggle="collapse"
-									  data-target="#main-menu">
-								<span class="sr-only">main menu</span>
-								<span class="glyphicon glyphicon-menu-hamburger"></span>
-							</button>
-							<a class="navbar-brand" href="/">
-								<i class="fa fa-home" aria-hidden="true"></i></a>
-							<ul class="nav navbar-nav navbar-right">
-								<li><a href="#"></a></li>
-								<li><a href="/portfolio">Portfolio</a></li>
-								<li><a href="/contact">Contact Me</a></li>
-							</ul>
-						</div>
-						<!-- here are your main nav links, grouped for toggling -->
-						<div class="collapse navbar-collapse" id="main-menu">
-							<a class="navbar-icon">
-								<p class="navbar-text navbar-right"></p>
-						</div>
-						<main>
-			</header>
+			<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+				<div class="container topnav">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand topnav" href="#">Diane Peshlakai</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<a href="#about">About</a>
+							</li>
+							<li>
+								<a href="#services">Services</a>
+							</li>
+							<li>
+								<a href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>
+
 			<!--Beginning row-->
 			<div class="container">
 				<div class="row">
